959f1f71813677baad25edc2b3de867614d864b6
Isabelle Ford <isabelleford11@gmail.com>
2022-04-27 10:44:57 +1000

    First commit

--- android/app/src/main/java/com/stopwatch/MainApplication.java
+++ android/app/src/main/java/com/stopwatch/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.List;
 
+//I'm literally just changing things
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
